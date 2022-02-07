23ff208b6b055c784231fdc6d59ad870945a412f
Hafner Stefan <stefan.hafner.development@gmail.com>
2022-02-07 09:48:31 +0100

    added exam

--- Bit.Tae21.TestApp/Program.cs
+++ Bit.Tae21.TestApp/Program.cs
@@ -8,8 +8,16 @@ namespace Bit.Tae21.TestApp
     {
         static void Main(string[] args)
         {
-            
+            // 1.) Eigenes Objekt anlegen {Nachname + Objektname} im Ornder Objects
+            // 2.) Eigene Liste in Program.cs in der Methode Main deklarieren
+            // 3.) Die eigene Liste mithilfe der foreach Methode ausgeben und debuggen
+            // 4.) Die eigene Liste mithilfe von einer Benutzereingabe erweitern
+            // 5.) Die Benutzereingaben dynamisch oft durchlaufen (z.B. do-while Schleife)
+            // 6.) Den Programmcode von Program.cs - Main in eine separate Klasse {Nachname + Main}
+            //     einfügen im Ordner "Main"
+            // 7.) Alle Änderungen auf GitHub hochladen
 
+            // Zielzeit: 09:35
         }
     }
 }
