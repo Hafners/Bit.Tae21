05873ce1bba9a2c9fffe458c6bf04b311eb014aa
Hafner Stefan <stefan.hafner.development@gmail.com>
2022-02-07 08:29:19 +0100

    added new program

---
+++ Bit.Tae21.TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bit.Tae21.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
